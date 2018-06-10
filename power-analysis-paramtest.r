b33d20cad0ab2081703be8ea2286b0d0428be313
Peter M. Dahlgren <peterdalle@users.noreply.github.com>
2018-06-10 17:41:03 +0200

    Variable format more uniform

--- power-analysis-paramtest.r
+++ power-analysis-paramtest.r
@@ -5,11 +5,10 @@ set.seed(1234)
 # Create a function with a linear model with:
 # - 2 independent variables
 # - 1 interaction
-lm_interaction <- function(simNum, n, b1, b2, b3, b0=0, x1m=0, x1sd=1,
-                                x2m=0, x2sd=1, alpha=.05) {
+lm_interaction <- function(n, b1, b2, b3, b0=0, x1.m=0, x1.sd=1, x2.m=0, x2.sd=1, alpha=.05) {
   # Populate data.
-  x1 <- rnorm(n, x1m, x1sd)
-  x2 <- rnorm(n, x2m, x2sd)
+  x1 <- rnorm(n, x1.m, x1.sd)
+  x2 <- rnorm(n, x2.m, x2.sd)
   
   # Residual variance.
   yvar <- sqrt(1 - b1^2 - b2^2 - b3^2)
@@ -29,7 +28,8 @@ lm_interaction <- function(simNum, n, b1, b2, b3, b0=0, x1m=0, x1sd=1,
   # Get interaction.
   int.b <- coef(summary(model))['x1:x2', 'Estimate']
   int.p <- coef(summary(model))['x1:x2', 'Pr(>|t|)']
-
+  
+  # Return as list.
   return(c(x1.b    = x1.b,
            x1.p    = x1.p,
            x1.sig  = x1.p < alpha,
@@ -41,10 +41,12 @@ lm_interaction <- function(simNum, n, b1, b2, b3, b0=0, x1m=0, x1sd=1,
            int.sig = int.p < alpha))
 }
 
-# Perform grid search simulation using 8 CPU cores, varying sample size and
-# using coefficient .25 and .15 of main effects and .10 for their interaction.
-power_lm_int <- grid_search(lm_interaction, params=list(n=seq(200, 900, by=50)),
-                            b1=.5, b2=.15, b3=.10, 
+# Grid search simulation using 8 CPU cores, increasing sample size with coefficients:
+# - main effect: .25 
+# - main effect: .15
+# - interaction: .10
+power_lm_int <- grid_search(lm_interaction, params=list(n=seq(50, 900, by=50)),
+                            b1=.25, b2=.15, b3=.10, 
                             n.iter=5000, output='data.frame', parallel='snow', ncpus=8)
 
 # Summarize power results.
@@ -60,16 +62,17 @@ power
 # Plot power graph.  
 power %>%
   gather("term", "power", "Main effect 1", "Main effect 2", "Interaction") %>%
-    ggplot(aes(x=n.test, y=power, group=factor(term), color=factor(term), linetype=factor(term))) +
-      geom_point() +
+    ggplot(aes(x=n.test, y=power, group=factor(term), color=factor(term), linetype=factor(term), shape=factor(term))) +
+      geom_point(size=3) +
       geom_line() +
-      scale_x_continuous(breaks=c(seq(0, 10000, by=200)), limits=c(200, 900)) +
+      scale_x_continuous(breaks=c(seq(0, 10000, by=200)), limits=c(50, 900)) +
       geom_hline(yintercept=0.95, alpha=0.3, linetype=2) +
       scale_y_continuous(breaks=c(seq(0, 1, by=0.1)), limits=c(0, 1)) +
       labs(title = paste0("Monte Carlo simulation (n=", NROW(power_lm_int$tests$iter)," tests) of power for linear model"),
            subtitle = "Dashed horizontal line indicate 95% power", 
-           x = 'Sample Size',
-           y = 'Power',
+           x = "Sample Size",
+           y = "Power",
            color = "Predictors",
-           linetype = "Predictors") +
+           linetype = "Predictors",
+           shape = "Predictors") +
       theme_minimal()
